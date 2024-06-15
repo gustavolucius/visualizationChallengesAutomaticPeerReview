acc65ca128b076c911fca7408a00de3c0351dde8
gustavolucius <62659698+gustavolucius@users.noreply.github.com>
2024-06-14 21:35:49 -0300

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Introduction",
+    page_icon="👋",
+)
+
+st.write("# Welcome! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Welcome to the data visualization of the 'Challenges for Automatic Peer Review Process' database!  
+    **👈 Select a visualization from the sidebar**
+
+    ### This project was developed by:    
+        - 💡 Gustavo Lúcius Fernandes
+        - 💡 Karen Stéfany Martins
+"""
+)
